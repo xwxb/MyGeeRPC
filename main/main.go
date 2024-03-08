03b25d9c18b084cbc58ad31fc5c9152008421ccb
xban <x_ban@qq.com>
2024-03-08 11:41:43 +0800

    day2: async and concurrency

--- main/main.go
+++ main/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	geerpc "github.com/xwxb/MyGeeRPC"
-	"github.com/xwxb/MyGeeRPC/codec"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -22,32 +21,26 @@ func startServer(addr chan string) {
 }
 
 func main() {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
-
-	// in fact, following code is like a simple geerpc client
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
+	client, _ := geerpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-
-	// 最过拟合难理解的点是，conn 本身就实现了 writer 是直接往里写的，这里不是抽象成了显示请求
-	// send options
-	// json encoder convert struct to json format and write to conn
-	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption) // return a encoder write to conn, and encode the DefaultOption
-	cc := codec.NewGobCodec(conn)                          // client side codec
 	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
+	wg.Wait()
 }
